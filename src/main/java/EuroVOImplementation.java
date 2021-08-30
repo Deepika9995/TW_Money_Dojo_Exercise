bdae98c55bf7ab8d097d8cdb9f486badf77733e0
Deepika <gjdeepika9@gmail.com>
2021-08-31 02:02:55 +0530

    Check equality of two Euro Value Objects

--- src/main/java/EuroVOImplementation.java
+++ src/main/java/EuroVOImplementation.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class EuroVOImplementation {
@@ -8,5 +9,25 @@ public class EuroVOImplementation {
             sumMoney += euroVO.getEuroAmount().getNumber().doubleValue();
         }
         return sumMoney;
+    }
+
+
+    public boolean findMatchingEuroVO(Double e1, Double e2, ArrayList<EuroVO> euroVOList) {
+        boolean foundMatch = false;
+        Double x = null;
+        Double sum = e1 + e2;
+        if (!(euroVOList.isEmpty())) {
+            for (EuroVO euroVO : euroVOList) {
+                if (sum.equals(euroVO.getEuroAmount().getNumber().doubleValue())) {
+                    foundMatch = true;
+                    x = euroVO.getEuroAmount().getNumber().doubleValue();
+                    break;
+                }
+            }
+            if (foundMatch) {
+                System.out.println("Sum equals the Object Value " + x);
+            }
+        }
+        return foundMatch;
     }
 }
--- src/test/java/test_EuroVO_Implementation.java
+++ src/test/java/test_EuroVO_Implementation.java
@@ -1,5 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,5 +20,30 @@ public class test_EuroVO_Implementation {
         Assert.assertEquals(50.0, euroVOImplementation.sumOfEuros(euroVOList), 0);
     }
 
+    @Test
+    public void test_Adding_Two_Euros_Should_Return_EuroVO() {
+        ArrayList<EuroVO> euroVOList = new ArrayList<>();
+        double[] d;
+        d = new double[]{2.0, 5.0, 7.0, 10.0, 10.0, 8.0, 8.0};
+        for (double v : d) {
+            EuroVO euroVO = new EuroVO();
+            euroVOList.add(euroVO.euroVO("EUR", v));
+        }
+        EuroVOImplementation euroVOImplementation = new EuroVOImplementation();
 
+        Double e1 = 2.0;
+        Double e2 = 5.0;
+
+        Assert.assertTrue(euroVOImplementation.findMatchingEuroVO(e1, e2, euroVOList));
+    }
+
+    @Test
+    public void test_Equality_Of_EuroVOS(){
+        EuroVO e1 = new EuroVO();
+        e1.euroVO("EUR",15.0);
+        EuroVO e2 = new EuroVO();
+        e2.euroVO("EUR", 15.0);
+        System.out.println(e1.equals(e2));
+        Assert.assertEquals("Equal", e1, e2);
+    }
 }
