d050f10f39d2c142abe4c0a147ed43dceca24e88
Deepika <gjdeepika9@gmail.com>
2021-08-30 21:01:21 +0530

    Check if valid currency code is passed

--- src/test/java/test_EuroVO.java
+++ src/test/java/test_EuroVO.java
@@ -3,13 +3,19 @@ import org.junit.Test;
 
 import javax.money.CurrencyUnit;
 import javax.money.Monetary;
+import javax.money.UnknownCurrencyException;
 
 public class test_EuroVO {
 
 @Test
-public void test_EuroVO_CurrencyUnit_Is_NotNULL(){
+public void euroVO_currencyUnit_is_notNULL(){
     CurrencyUnit eur = Monetary.getCurrency("EUR");
     Assert.assertNotNull(eur);
+}
+
+@Test(expected = UnknownCurrencyException.class)
+public void throwsError_currencyCode_whenNoExist(){
+    Monetary.getCurrency("XYZ");
 }
 
 }
